2b32ff9d29d1bd9c296dc5170e05d27d6348a463
Mill Daulagala <macos@macOSs-MBP.modem>
2021-10-16 22:28:56 +1100

    Initialize the goodMessage with Spring Boot

---
+++ src/main/java/com/mill/goodMessage/GoodMessageApplication.java
@@ -0,0 +1,13 @@
+package com.mill.goodMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mill/goodMessage/GoodMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mill.goodMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
